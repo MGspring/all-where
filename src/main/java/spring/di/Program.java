b2d5b27622488bcbece3060554090abfe0ad99ba
MGspring <wlsaudrhkd1@gmail.com>
2023-01-25 15:30:35 +0900

    1.0.2

--- src/main/java/spring/di/Program.java
+++ src/main/java/spring/di/Program.java
@@ -1,14 +1,20 @@
 package spring.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
 import spring.di.ui.ExamConsole;
 
 public class Program {
 	public static void main(String[] args) {
 
 		// Exam exam = new NewlecExam();
+		// Exam exam = new NewlecExam(10, 10, 10, 10);
 		// ExamConsole console = new GridExamConsole(exam);
 		//
 		// console.setExam(exam);
@@ -16,9 +22,17 @@ public class Program {
 		ApplicationContext context =
 			new ClassPathXmlApplicationContext("setting.xml");
 
+		Exam exam = context.getBean(Exam.class);
+		System.out.println(exam.toString());
 		ExamConsole console = (ExamConsole)context.getBean("console");
 		// ExamConsole console = context.getBean(ExamConsole.class);
 		console.print();
+
+		List<Exam> exams = (List<Exam>)context.getBean("exams"); //new ArrayList<>();
+		exams.add(new NewlecExam(1, 1, 1, 1));
+
+		for(Exam e : exams)
+			System.out.println(e);
 
 	}
 }
